271657d8e7b05b19768fde69e289ccc2087c4fbc
sabdahtb <sabdahutabarat2804@gmail.com>
2022-09-16 20:03:33 +0700

    add cors to integrate with react

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/joho/godotenv v1.4.0
 	github.com/mashingan/smapping v0.1.18
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sabdahtb/config"
 	"github.com/sabdahtb/controller"
@@ -20,6 +23,18 @@ var (
 func main() {
 	defer config.CloseDBConnection(db)
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "*"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	authRoutes := r.Group("api/auth")
 	{
